b9a5fa07c5be456ddc198abc03ccfdfe75ee95a1
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2023-03-19 15:08:24 -0700

    Refactor dimacs/parse.rs

--- src/dimacs/parse.rs
+++ src/dimacs/parse.rs
@@ -2,6 +2,7 @@
 /// DIMACS CNF file parser
 ///
 use crate::clause::Clause;
+use crate::dimacs::cnf::Cnf;
 use crate::lit::Lit;
 
 use std::ffi::OsStr;
@@ -9,8 +10,7 @@ use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
 
-use crate::dimacs::cnf::Cnf;
-use anyhow::{bail, Result};
+use anyhow::{bail, Context, Result};
 
 // The output is wrapped in a Result to allow matching on errors
 // Returns an Iterator to the Reader of the lines of the file.
@@ -39,44 +39,42 @@ impl Parse for Path {
         // File hosts must exist in current path before this produces output
         if let Ok(lines) = read_lines(self) {
             // Consumes the iterator, returns an (Optional) String
-            for line in lines {
-                if let Ok(ln) = line {
-                    // c start with comments
-                    if ln.is_empty() || ln.starts_with('c') {
-                        continue;
-                    }
+            for line in lines.into_iter().flatten() {
+                // c start with comments
+                if line.is_empty() || line.starts_with('c') {
+                    continue;
+                }
 
-                    let vars: Vec<&str> = ln.split_whitespace().collect();
+                let vars: Vec<&str> = line.split_whitespace().collect();
 
-                    // p cnf 5 3
-                    if vars[0] == "p" && vars[1] == "cnf" {
-                        if vars.len() != 4 {
-                            bail!("Must have 2 arguments: {ln}");
-                        }
-                        cnf.num_vars = vars[2].parse()?;
-                        cnf.num_clauses = vars[3].parse()?;
-                        continue;
+                // p cnf 5 3
+                if vars[0] == "p" && vars[1] == "cnf" {
+                    if vars.len() != 4 {
+                        bail!("Must have 2 arguments: {line}");
                     }
+                    cnf.num_vars = vars[2].parse()?;
+                    cnf.num_clauses = vars[3].parse()?;
+                    continue;
+                }
 
-                    // 1 -5 4 0
-                    let mut clause = Clause::default();
-                    for var in vars {
-                        let v = var.parse::<i32>().expect(&format!(
-                            "var `{var}` should formatted as i32, in line: {ln}"
-                        ));
-                        if v == 0 {
-                            break;
-                        }
-                        let num = v.abs() - 1;
-                        if v > 0 {
-                            clause.push(Lit::new(num, false));
-                        } else {
-                            clause.push(Lit::new(num, true));
-                        }
+                // 1 -5 4 0
+                let mut clause = Clause::default();
+                for var in vars {
+                    let v = var.parse::<i32>().context(format!(
+                        "var `{var}` should formatted as i32, in line: {line}"
+                    ))?;
+                    if v == 0 {
+                        break;
                     }
-                    if !clause.is_empty() {
-                        cnf.clauses.push(clause);
+                    let num = v.abs() - 1;
+                    if v > 0 {
+                        clause.push(Lit::new(num, false));
+                    } else {
+                        clause.push(Lit::new(num, true));
                     }
+                }
+                if !clause.is_empty() {
+                    cnf.clauses.push(clause);
                 }
             }
         }
